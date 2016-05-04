4b2935282df8ac90f1f869aa5f3a267c8d4b24dd
wuzhy <313388925@qq.com>
2016-05-04 15:27:50 +0800

    添加注释

--- kinth-frame-manage-web/src/com/kinth/interceptor/shiro/ShiroRealm.java
+++ kinth-frame-manage-web/src/com/kinth/interceptor/shiro/ShiroRealm.java
@@ -18,6 +18,7 @@ public class ShiroRealm extends AuthorizingRealm {
 	/*
 	 * 登录信息和用户验证信息验证(non-Javadoc)
 	 * @see org.apache.shiro.realm.AuthenticatingRealm#doGetAuthenticationInfo(org.apache.shiro.authc.AuthenticationToken)
+	 *身份认证/登录，验证用户是不是拥有相应的身份；
 	 */
 	@Override
 	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
@@ -33,6 +34,7 @@ public class ShiroRealm extends AuthorizingRealm {
 	/*
 	 * 授权查询回调函数, 进行鉴权但缓存中无用户的授权信息时调用,负责在应用程序中决定用户的访问控制的方法(non-Javadoc)
 	 * @see org.apache.shiro.realm.AuthorizingRealm#doGetAuthorizationInfo(org.apache.shiro.subject.PrincipalCollection)
+	 *授权，即权限验证，验证某个已认证的用户是否拥有某个权限；即判断用户是否能做事情，常见的如：验证某个用户是否拥有某个角色。或者细粒度的验证某个用户对某个资源是否具有某个权限；
 	 */
 	@Override
 	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection pc) {
